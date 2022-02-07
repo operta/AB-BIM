ccb8b5dc3ed68c8f609dc3d7f7f20a1ce61737f6
dzan.operta <dzan.operta@tuwien.ac.at>
2022-02-07 23:01:03 +0100

    pep format

--- MSecondExtension.extension/As-Built Tunnel BIM.tab/Model Data.panel/Load from TIMS.pushbutton/script.py
+++ MSecondExtension.extension/As-Built Tunnel BIM.tab/Model Data.panel/Load from TIMS.pushbutton/script.py
@@ -3,6 +3,7 @@
 import requests
 import credentials
 
+
 def authenticate():
     response = requests.post("https://tunnel.big.tuwien.ac.at:8000/api/login/",json={"user": credentials.username, "password": credentials.password})
     access_token = "Bearer " + response.text
